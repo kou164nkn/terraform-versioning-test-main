283536afa6c9aee4a56a3867ebb376953ed28181
Hiroshi Nakano <kou164nkn@gmail.com>
2022-03-26 23:28:47 +0900

    Updeated

--- main.tf
+++ main.tf
@@ -16,9 +16,20 @@ module "example-vpc-v020" {
   vpc_cidr = "20.0.0.0/16"
 }
 
-module "remote-example-vpc-v010" {
-  source = "git@github.com:kou164nkn/terraform-versioning-test-modules.git?ref=v0.1.0"
+module "remote-example-vpc-v060-manual" {
+  source = "git@github.com:kou164nkn/terraform-versioning-test-modules.git//test-module2?ref=v0.6.0"
 
-  vpc_name = "test-vpc-1-remote"
-  vpc_cidr = "30.0.0.0/16"
+  manual_setting_flag = true
+  vpc_name            = "test-vpc-1-remote"
+  vpc_cidr            = "30.0.0.0/16"
+  enable_dns_support  = true
+}
+
+module "remote-example-vpc-v060-IaC" {
+  source = "git@github.com:kou164nkn/terraform-versioning-test-modules.git//test-module2?ref=v0.6.0"
+
+  manual_setting_flag = false
+  vpc_name            = "test-vpc-1-remote"
+  vpc_cidr            = "30.0.0.0/16"
+  enable_dns_support  = true
 }
